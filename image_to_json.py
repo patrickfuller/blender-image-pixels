f1a05307c443fda2e2ee45a0d11f9efe0271e7b7
Rodolfo Oliveira <oliveira.rodolfo87@gmail.com>
2015-03-20 13:15:48 -0300

    Update image_to_json script
    The script now runs on `-i IMAGE.ext -o FILE.json` basis and no longer
    defaults to `pixels.json`. Next modification should input a file to
    blender export.

--- image_to_json.py
+++ image_to_json.py
@@ -1,23 +1,72 @@
 #!/usr/bin/env python
+"""Converts image to json-like file.
 """
-Opens image and converts to csv data using the PIL library
-"""
+
+
+import os
+import sys
+import getopt
 
 import Image
-from sys import argv, exit
 import json
 
-if len(argv) != 2:
-    print "USAGE: python image_to_csv.py *image_name*"
-    exit()
 
-image = Image.open(argv[1])
-pixels = list(image.getdata())
-width, height = image.size
-pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
+params = {'sname': os.path.basename(sys.argv[0]),
+          'h':('-h', '--help'),
+          'i':', '.join(('-i IMAGE', '--input=IMAGE')),
+          'idesc': 'Input image file, parsed using PIL library',
+          'o':', '.join(('-o IMAGE', '--output=IMAGE')),
+          'odesc': 'Output file, *.json',
+          'fill':' ',
+          'align':'<',
+          'newline':'\n',
+          'width':30,
+          'start':4*' ',
+          }
 
-# Use first value if multi channel
-if type(pixels[0][0]) is not int:
-    pixels = [[p[0] for p in row] for row in pixels]
+def help_msg():
+    s = ('{newline}Usage: {sname} [options]{newline}'
+         '{newline}Options:'
+         '{newline}{start}{i:{fill}{align}{width}s}{idesc}'
+         '{newline}{start}{o:{fill}{align}{width}s}{odesc}').format(**params)
+    return s
 
-print json.dumps(pixels, indent=4)
+
+def get_pixels(fname):
+    image = Image.open(fname)
+    pixels = list(image.getdata())
+    width, height = image.size
+    pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
+    if type(pixels[0][0]) is not int:
+        pixels = [[p[0] for p in row] for row in pixels]
+    return pixels
+
+def write_json(fname, pixels):
+    with open(fname, 'w') as f:
+        return f.write(json.dumps(pixels, indent=4))
+
+if __name__ == '__main__':
+    ifile = ''
+    ofile = ''
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'hi:o:',
+                                   ['help', 'input=', 'output='])
+
+    except getopt.GetoptError as err:
+        print str(err)
+        print help_msg()
+        sys.exit(1)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print help_msg()
+            sys.exit(0)
+
+        elif opt in ('-i', '--input'):
+            ifile = arg
+
+        elif opt in ('-o', '--output'):
+            ofile = arg
+
+    write_json(ofile, get_pixels(ifile))
