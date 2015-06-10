01ff15c6909c51bc88d0fcdf26276f61e9615c56
Rodolfo Oliveira <oliveira.rodolfo87@gmail.com>
2015-06-10 15:17:12 -0300

    Updating #1 suggestions from @patrickfuller
    Got rid of `docopt` in favor of `argparse`;
    Got rid of `get_filter` in favor of `getattr`;
    
    :+1: `yapf` formatting for PEP8 style

--- image_to_json.py
+++ image_to_json.py
@@ -1,49 +1,40 @@
 #!/usr/bin/env python
-"""Converts image to json-like file.
+"""usage: image_to_json.py [-h] -i INPUT [-o OUTPUT] [-s SIZE] [-f FILTER]
 
-Usage:
-    image_to_json.py (-i FILE) [-o FILE] [-s K] [-f FILTER]
+Converts image to json file.
 
-Options:
-    -h --help                   Shows this screen.
-    -i FILE --input=FILE        Input image file.
-    -o FILE --output=FILE       Output json file. [default: image.json]
-    -s K --size=K               Resize image in K. [default: False]
-    -f FILTER --filter=FILTER   Resampling filter, NEAREST, BILINEAR, BICUBIC or LANCZOS. [default: NEAREST]
+optional arguments:
+  -h, --help            show this help message and exit
+  -i INPUT, --input INPUT
+                        Input image file.
+  -o OUTPUT, --output OUTPUT
+                        Output json file.
+  -s SIZE, --size SIZE  Reduce image by a proportional factor.
+  -f FILTER, --filter FILTER
+                        Resampling filter. Can be 'nearest', 'bilinear',
+                        'bicubic', or 'lanczos'.
+
 """
 
-
-import os
-import sys
-import getopt
-from docopt import docopt
-
+import argparse
 import json
 
 from PIL import Image
 
 
-def get_filter(resample):
-    if resample.upper() == 'NEAREST':
-        f = Image.NEAREST
-    elif resample.upper() == 'BILINEAR':
-        f = Image.BILINEAR
-    elif resample.upper() == 'BICUBIC':
-        f = Image.BICUBIC
-    elif resample.upper() == 'LANCZOS':
-        f = Image.LANCZOS
-    else:
-        f = Image.NEAREST
-    return f
-
-def get_pixels(fname, size=False, resample='NEAREST'):
+def get_pixels(fname, size=1, resample='nearest'):
     image = Image.open(fname)
     width, height = image.size
 
-    if size.upper() != 'FALSE':
-        width = int(width*float(size))
-        height = int(height*float(size))
-        image = image.resize((width, height), resample=get_filter(resample))
+    if size <= 0:
+        raise ValueError("Size must be a positive value.")
+    elif size > 1:
+        raise ValueError("Size is specified as a proportion, not a percent.")
+    elif size != 1:
+        width = int(width * float(size))
+        height = int(height * float(size))
+        image = image.resize((width, height),
+                             resample=getattr(Image, resample.upper()))
 
     pixels = list(image.getdata())
     pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
@@ -53,13 +44,32 @@ def get_pixels(fname, size=False, resample='NEAREST'):
 
     return pixels
 
+
 def write_json(fname, pixels):
     with open(fname, 'w') as f:
         return f.write(json.dumps(pixels, indent=4))
 
+
 if __name__ == '__main__':
-    args = docopt(__doc__)
-    img = get_pixels(args['--input'],
-                     size=args['--size'],
-                     resample=args['--filter'])
-    write_json(args['--output'], img)
+    parser = argparse.ArgumentParser(description="Converts image to json file.")
+    parser.add_argument('i', '--input',
+                        type=str,
+                        required=True,
+                        help="Input image file.")
+    parser.add_argument('-o', '--output',
+                        type=str,
+                        default='image.json',
+                        help="Output json file.")
+    parser.add_argument('-s', '--size',
+                        type=float,
+                        default=1,
+                        help="Reduce image by a proportional factor.")
+    parser.add_argument('-f', '--filter',
+                        type=str,
+                        default='nearest',
+                        help=("Resampling filter. Can be 'nearest', "
+                              "'bilinear', 'bicubic', or 'lanczos'."))
+    args = parser.parse_args()
+
+    img = get_pixels(args.input, size=args.size, resample=args.filter)
+    write_json(args.output, img)
